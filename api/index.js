a6b0c6e1bc3e0de2fdf9ee75ea342a7d7695f05e
shashankBgowda <shashankbg74@gmail.com>
2025-04-08 12:58:43 -0400

    create test API route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Userroute from "./routes/user.routes.js";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
-}).catch((error)=>{
-    console.log(error,"somenthing went wrong in connecting to database");
+}).catch((error) => {
+    console.log(error, "Something went wrong in connecting to database");
 });
 
-app.listen(1900,()=>{
-    console.log("Server listing to Port 1900!");
+
+app.listen(2908, () => {
+    console.log("Server listening to Port 2908!");
 });
+
+app.use("/api/user", Userroute);
