40da3757b73c0e4505271949abd9044624637ea1
shashankBgowda <shashankbg74@gmail.com>
2025-04-08 10:17:14 -0400

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app =express();
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((error)=>{
+    console.log(error,"somenthing went wrong in connecting to database");
+});
+
 app.listen(1900,()=>{
     console.log("Server listing to Port 1900!");
 });
